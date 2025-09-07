3d4900483b7b4efb5cddd856a00b19701551b16e
Saul Gustavo <saulgustavo237@gmail.com>
2025-09-06 20:14:34 -0600

    Se cambio el diseño del CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,19 +2,17 @@ import BtnDescarga from "./BtnDescarga";
 
 export default function CTA() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen">
-      <div className="rounded-4xl text-center bg-gradient-to-b from-(--color-purple) to-(--color-cyan) py-10 px-5 mx-4 lg:px-10 lg:py-30">
-        <h3 className="mb-4 font-bold text-2xl md:text-4xl">
-          ¿Listo para encender tu creatividad
-        </h3>
-        <p className="mb-4 font-semibold">
-          Únete a miles de mentes creativas que ya están desbloqueando su
-          potencial. Descarga Spark hoy mismo y empieza gratis.
-        </p>
-        <span className="mb-4">Descarga Spark (Es Gratis)</span>
-        <div className="flex justify-center">
-          <BtnDescarga />
-        </div>
+    <section className="flex flex-col items-center justify-center text-center bg-gradient-to-b from-(--color-purple) to-(--color-cyan) py-20 px-4">
+      <h3 className="mb-4 font-bold text-2xl md:text-4xl">
+        ¿Listo para encender tu creatividad
+      </h3>
+      <p className="mb-4 font-semibold">
+        Únete a miles de mentes creativas que ya están desbloqueando su
+        potencial. Descarga Spark hoy mismo y empieza gratis.
+      </p>
+      <span className="mb-4">Descarga Spark (Es Gratis)</span>
+      <div className="flex justify-center">
+        <BtnDescarga />
       </div>
     </section>
   );
