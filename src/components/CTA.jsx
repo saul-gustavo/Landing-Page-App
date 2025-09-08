169e69958cd9ecbbb22a2345d628b27712fa0896
Saul Gustavo <saulgustavo237@gmail.com>
2025-09-08 14:03:19 -0600

    Se modifico el diseño del CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,7 @@ import BtnDescarga from "./BtnDescarga";
 
 export default function CTA() {
   return (
-    <section className="flex flex-col items-center justify-center text-center bg-gradient-to-b from-(--color-purple) to-(--color-cyan) py-20 px-4">
+    <section className="flex flex-col items-center justify-center text-center bg-gradient-to-b from-(--color-purple) to-(--color-cyan) py-30 px-4">
       <h3 className="mb-4 font-bold text-2xl md:text-4xl">
         ¿Listo para encender tu creatividad
       </h3>
